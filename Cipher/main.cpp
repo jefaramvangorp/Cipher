d77164804bda9d44b269582898fbd13f55e8693f
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-02-06 16:58:07 +0100

    Initial commit of main.

--- Cipher/main.cpp
+++ Cipher/main.cpp
@@ -1,15 +1,9 @@
-//
-//  main.cpp
-//  Cipher
-//
-//  Created by Jef-Aram Van Gorp on 06/02/15.
-//  Copyright (c) 2015 Jef-Aram Van Gorp. All rights reserved.
-//
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+
+int main()
+{
+
     return 0;
 }

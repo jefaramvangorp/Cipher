caa2c5c8a1a12be71429539706a86756743542c4
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-02-09 17:12:48 +0100

    Add alphabet interface.

---
+++ Cipher/Algorithm/Alphabet.h
@@ -0,0 +1,12 @@
+
+#ifndef CIPHER_ALGORITHM_ALPHABET_H_
+#define CIPHER_ALGORITHM_ALPHABET_H_
+
+class Alphabet
+{
+public:
+    virtual ~Alphabet() {}
+    
+};
+
+#endif // CIPHER_ALGORITHM_ALPHABET_H_

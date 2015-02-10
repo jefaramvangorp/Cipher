4b312f7b325ac5b0bd56559bcf979fafa10831b0
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-02-10 07:35:26 +0100

    Add application class.

---
+++ Cipher/Application/CipherApp.cpp
@@ -0,0 +1,9 @@
+//
+//  CipherApp.cpp
+//  Cipher
+//
+//  Created by Jef-Aram Van Gorp on 09/02/15.
+//  Copyright (c) 2015 Jef-Aram Van Gorp. All rights reserved.
+//
+
+#include "CipherApp.h"
---
+++ Cipher/Application/CipherApp.h
@@ -0,0 +1,14 @@
+//
+//  CipherApp.h
+//  Cipher
+//
+//  Created by Jef-Aram Van Gorp on 09/02/15.
+//  Copyright (c) 2015 Jef-Aram Van Gorp. All rights reserved.
+//
+
+#ifndef __Cipher__CipherApp__
+#define __Cipher__CipherApp__
+
+#include <stdio.h>
+
+#endif /* defined(__Cipher__CipherApp__) */

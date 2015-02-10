b39cdd3628e8b1f76a3ba6676c6bb421cd0242f3
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-02-10 07:46:29 +0100

    Add class to handle File I/O.

---
+++ Cipher/IO/FileIO.cpp
@@ -0,0 +1,9 @@
+//
+//  FileIO.cpp
+//  Cipher
+//
+//  Created by Jef-Aram Van Gorp on 10/02/15.
+//  Copyright (c) 2015 Jef-Aram Van Gorp. All rights reserved.
+//
+
+#include "FileIO.h"
---
+++ Cipher/IO/FileIO.h
@@ -0,0 +1,14 @@
+//
+//  FileIO.h
+//  Cipher
+//
+//  Created by Jef-Aram Van Gorp on 10/02/15.
+//  Copyright (c) 2015 Jef-Aram Van Gorp. All rights reserved.
+//
+
+#ifndef __Cipher__FileIO__
+#define __Cipher__FileIO__
+
+#include <stdio.h>
+
+#endif /* defined(__Cipher__FileIO__) */

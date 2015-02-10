e21010001fb81f190a6e14ebc3b3787826d48cbd
Jef-Aram Van Gorp <jefaram.vangorp@refractor.be>
2015-02-10 07:47:26 +0100

    Use FileIO class in CipherApp.

--- Cipher/Application/CipherApp.cpp
+++ Cipher/Application/CipherApp.cpp
@@ -3,50 +3,10 @@
 #include "CipherApp.h"
 #include "Algorithm/VigenereCipher.h"
 #include "UI/UI.h"
+#include "IO/FileIO.h"
 
 // STD.
-#include <fstream>
-#include <sstream>
 #include <iostream>
-
-namespace {
-    void readTextFile(const std::string& fileName, std::string* text)
-    {
-        std::ifstream in(fileName);
-        if (in.is_open())
-        {
-            std::string line;
-            std::ostringstream stream;
-            
-            while (std::getline(in, line))
-            {
-                stream << line << "\n";
-            }
-            
-            (*text) = stream.str();
-            in.close();
-        }
-        else
-        {
-            fprintf(stderr, "Could not open file: %s\n", fileName.c_str());
-        }
-    }
-    
-    void writeTextFile(const std::string& fileName, const std::string& text)
-    {
-        std::ofstream out(fileName);
-        if (out.is_open())
-        {
-            out << text;
-            out.close();
-        }
-        else
-        {
-            fprintf(stderr, "Could not open file: %s\n", fileName.c_str());
-        }
-    }
-}
-
 
 CipherApp::CipherApp(UI* ui)
 : ui(ui)
@@ -77,12 +37,12 @@ void CipherApp::encrypt()
     std::string key = this->ui->getTextFromUser("Enter key:");
     
     std::string message;
-    readTextFile(file_name.c_str(), &message);
+    FileIO::readTextFile(file_name.c_str(), &message);
     
     VigenereCipher cipher(key);
     std::string encrypted_message = cipher.encrypt(message);
     
-    writeTextFile(file_name, encrypted_message);
+    FileIO::writeTextFile(file_name, encrypted_message);
 }
 
 void CipherApp::decrypt()
@@ -91,12 +51,12 @@ void CipherApp::decrypt()
     std::string key = this->ui->getTextFromUser("Enter key:");
     
     std::string message;
-    readTextFile(file_name, &message);
+    FileIO::readTextFile(file_name, &message);
     
     VigenereCipher cipher(key);
     std::string decrypted_message = cipher.decrypt(message);
     
-    writeTextFile(file_name, decrypted_message);
+    FileIO::writeTextFile(file_name, decrypted_message);
 }
 
 void CipherApp::showHelp() const
